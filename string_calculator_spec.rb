f5623c69869a142454b4bfb4d061da815c344017
sonikamaliya <sonikamaliya@gmail.com>
2025-01-06 05:33:51 +0530

    Write unit test case and code for add method of string calculator class

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,5 +4,14 @@ require 'rspec'
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
+  describe 'String Calculator' do
 
+    before(:each) do
+      subject { StringCalculator.new }
+    end
+
+    it "Initilize number string with add method" do 
+      expect(subject.class.instance_methods).to include(:add)
+    end
+  end
 end
